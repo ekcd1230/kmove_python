3dd6ccb243af6537e04235163075f710e53ee154
ekcd1230 <ekcd1230@hanmail.net>
2019-07-16 17:56:03 +0900

    20190716

--- basic/random_test.py
+++ basic/random_test.py
@@ -1,5 +1,8 @@
 import random
 listLotto=[0,0,0,0,0,0]
 
-for i in range(0,5):
-    print(random.randint(1,45),end="    ")
+for i in range(0,6):
+    listLotto.pop(i)
+    listLotto.insert(i,random.randint(1,45))
+
+print(listLotto)
